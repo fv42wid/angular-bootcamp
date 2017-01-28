d682731de8354339a50fd5714c13143273d53134
Frank Vinci <fv42wid@yahoo.com>
2017-01-28 16:10:12 -0500

    finished bootcamp

---
+++ app/proposal/proposal-list.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Rx';
+import { Proposal } from './proposal';
+import { ProposalService } from './proposal.service';
+
+@Component({
+  moduleId: module.id,
+  selector: 'proposal-list',
+  templateUrl: 'proposal-list.component.html',
+  styleUrls: ['proposal-list.component.css'],
+  providers: [ProposalService]
+})
+export class ProposalListComponent implements OnInit {
+
+  proposals: Proposal[] = []
+  errorMessage: string;
+  mode = "Observable";
+
+  constructor(
+    private proposalService: ProposalService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    let timer = Observable.timer(0, 5000);
+    timer.subscribe(() => this.getProposals());
+  }
+
+  getProposals() {
+    this.proposalService.getProposals()
+        .subscribe(
+          proposals => this.proposals = proposals,
+          error => this.errorMessage = <any>error
+        );
+  }
+
+  goToShow(proposal: Proposal): void {
+    let link = ['/proposal', proposal.id];
+    this.router.navigate(link);
+  }
+
+  /*
+  proposalOne: Proposal = new Proposal(15, 'Abc Co', 'http://jordan.com', 'Ruby on Rails', 150, 120, 15, 'jordan@test.com')
+  proposalTwo: Proposal = new Proposal(99, 'Xyz Co', 'http://jordan.com', 'Ruby on Rails', 150, 120, 15, 'jordan@test.com')
+  proposalThree: Proposal = new Proposal(300, '123 Co', 'http://jordan.com', 'Ruby on Rails', 150, 120, 15, 'jordan@test.com')
+  */
+
+
+}
+
---
+++ app/proposal/proposal.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+
+import { Proposal } from './proposal';
+
+@Injectable()
+export class ProposalService{
+  private proposalsUrl = 'http://35.165.61.165/proposals';
+
+  constructor(
+  private http: Http
+  ) {}
+
+  getProposals(): Observable<Proposal[]> {
+    return this.http.get(this.proposalsUrl)
+           .map((response: Response) => <Proposal[]>response.json())
+           .catch(this.handleError);
+  }
+
+  getProposal(id: number): Observable<Proposal> {
+    return this.http.get(this.proposalsUrl + "/" + id + ".json");
+  }
+
+  createProposal(proposal) {
+    let headers = new Headers({ 'Content-Type': 'application/json'});
+    let options = new RequestOptions({ headers: headers });
+    return this.http.post(this.proposalsUrl, JSON.stringify(proposal), { headers: headers }).map(((res: Response) => res.json());
+  }
+
+  private handleError(error: Response | any) {
+    let errMsg: string;
+    if(error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+    errMsg = error.message ? error.message : error.toString();
+    }
+  }
+}
